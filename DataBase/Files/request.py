633622620e55fb9c904e63e2b595011c23a70149
ILikeWomenYes <88403710+ILikeWomenYes@users.noreply.github.com>
2021-12-23 19:28:15 +0000

    Update request.py

--- DataBase/Files/request.py
+++ DataBase/Files/request.py
@@ -1,8 +1,18 @@
 import index
 import time as t
 import os
+import time
 
 print("Welcome")
+print("to create a file do _new and NAME : will show up")
+print("type in your file name afterwards.")
+print("do _edit to edit a file, bare in mind that you have to type in the .filetype")
+print("the file type the database uses .dtb therefore if you wish to edit .json files for example")
+print("it wont edit the text it would just be (again for example pourpeses): EXAMPLE TEXT")
+print("_read will read files")
+print("_del deletes files")
+
+time.sleep(1.5)
 
 while 3 > 2:
     t.sleep(1)
@@ -23,3 +33,4 @@ while 3 > 2:
         os.remove(inp.lower())
     if inputbar == "_exit":
         break
+

aeceaa7c65b8b7de2280aa405be0b98e2f97124a
Code Man <88403710+ILikeWomenYes@users.noreply.github.com>
2021-12-24 10:16:59 +0000

    Update request.py

--- DataBase/Files/request.py
+++ DataBase/Files/request.py
@@ -7,7 +7,7 @@ print("Welcome")
 print("to create a file do _new and NAME : will show up")
 print("type in your file name afterwards.")
 print("do _edit to edit a file, bare in mind that you have to type in the .filetype")
-print("the file type the database uses .dtb therefore if you wish to edit .json files for example")
+print("File types need to be typed in like: .json, .xml, and need to be manualy typed as you would normally")
 print("it wont edit the text it would just be (again for example pourpeses): EXAMPLE TEXT")
 print("_read will read files")
 print("_del deletes files")
@@ -19,7 +19,8 @@ while 3 > 2:
     inputbar = input("SETTINGS: ")
     if inputbar == "_new":
         inp = input("NAME : ")
-        index.files.new(inp.lower() + ".dtb")
+        ftype = input("FILE TYPE : ")
+        index.files.new(inp.lower() + ftype)
     if inputbar == "_edit":
         inp = input("FILE NAME : ")
         checkFile = os.path.isfile(inp)
